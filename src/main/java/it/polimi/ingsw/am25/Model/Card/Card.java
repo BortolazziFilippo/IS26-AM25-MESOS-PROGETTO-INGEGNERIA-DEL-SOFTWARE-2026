b4337648efe76ad41cee2feae9cf3e211f4e9d96
Filippo Bortolazzi <filippo.bortolazzi@mail.polimi.it>
2026-03-14 16:05:27 +0100

    creata Card e InventorCard

--- src/main/java/it/polimi/ingsw/am25/Model/Card/Card.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Card/Card.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.am25.Model.Card;
 
+import it.polimi.ingsw.am25.Model.Enums.ERA;
+
 public abstract class Card {
+    private ERA era;
 
 }
---
+++ src/main/java/it/polimi/ingsw/am25/Model/Card/InventorCard.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.am25.Model.Card;
+
+import it.polimi.ingsw.am25.Model.Enums.INV_ICON;
+
+public class InventorCard extends Card{
+    private INV_ICON invIcon;
+
+
+
+}
