34980ed421e098487fd732b7ea3d2577c6fc713c
Filippo Bortolazzi <filippo.bortolazzi@mail.polimi.it>
2026-03-23 11:46:09 +0100

    Implementato e testato effetto FiveFoodCompletedSet

--- src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/FiveFoodCompletedSet.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/FiveFoodCompletedSet.java
@@ -1,13 +1,72 @@
 package it.polimi.ingsw.am25.Model.Effect.Building;
 
+import it.polimi.ingsw.am25.Model.Card.Card;
+import it.polimi.ingsw.am25.Model.Enums.CARD_TYPE;
 import it.polimi.ingsw.am25.Model.Player.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+//1
+//2
+//3
+//4
+//5
+//6
 public class FiveFoodCompletedSet extends BuildingEffect {
+    private boolean alreadyUsed = false;
+    private List<Boolean> setCard = new ArrayList<>();
+    private List<Card> listOldCard;
+    private List<Card> listOfNewCard;
+    private List<Card> difference;
     public FiveFoodCompletedSet() {
     }
 
     @Override
     public void applyEffect(Player player) {
 
+        if(!alreadyUsed){
+            for (int i = 0; i < 6; i++) {
+                setCard.add(i,false);
+            }
+
+            listOldCard=new ArrayList<>(player.getTribe());
+            this.alreadyUsed=true;
+            return;
+        }else{
+            listOfNewCard=player.getTribe();
+            difference=new ArrayList<>(listOfNewCard);
+            difference.removeAll(listOldCard);
+            listOldCard=new ArrayList<>(listOfNewCard);
+            for (Card card:difference){
+                switch (card.getCardType()){
+                    case BUILDER:
+                        setCard.set(0,true);
+                        break;
+                    case ARTIST:
+                        setCard.set(1,true);
+                        break;
+                    case GATHERER:
+                        setCard.set(2,true);
+                        break;
+                    case SHAMAN:
+                        setCard.set(3,true);
+                        break;
+                    case INVENTOR:
+                        setCard.set(4,true);
+                        break;
+                    case HUNTER:
+                        setCard.set(5,true);
+                        break;
+                    default:
+                        System.err.println(getClass()+" errore identificazione carta");
+                }
+            }
+            if(setCard.contains(false)){
+                return;
+            }else {
+                player.manageFood(+5);
+                setCard.replaceAll(element->false);
+            }
+        }
     }
 }
---
+++ src/test/java/it/polimi/ingsw/am25/Model/Effect/Building/FiveFoodCompletedSetTest.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.am25.Model.Effect.Building;
+
+import it.polimi.ingsw.am25.Model.Card.ArtistCard;
+import it.polimi.ingsw.am25.Model.Card.BuildingCard;
+import it.polimi.ingsw.am25.Model.Enums.CARD_TYPE;
+import it.polimi.ingsw.am25.Model.Enums.COLOR;
+import it.polimi.ingsw.am25.Model.Enums.ERA;
+import it.polimi.ingsw.am25.Model.Enums.EVENT_TYPE;
+import it.polimi.ingsw.am25.Model.Player.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiveFoodCompletedSetTest {
+    @Test
+    public void TestApplyEffect() {
+        Player player = new Player("Lorem Ipsum", COLOR.RED);
+        player.manageFood(5);
+        BuildingCard buildingCard= new BuildingCard(ERA.ERA_III, CARD_TYPE.GATHERER,1,10,10, EVENT_TYPE.END_ROUND);
+        buildingCard.setBuildingEffect(new FiveFoodCompletedSet());
+        buildingCard.applyEventEffect(player);
+        assertEquals(5,player.getFood());
+        //for semplicity i used the same class but changed type of card
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.ARTIST));
+        buildingCard.applyEventEffect(player);
+        assertEquals(5,player.getFood());
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.GATHERER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.BUILDER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.SHAMAN));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.HUNTER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.INVENTOR));
+        buildingCard.applyEventEffect(player);
+        assertEquals(10,player.getFood()); //this confirms the "each set completed add five food"
+
+
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.GATHERER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.BUILDER));
+        buildingCard.applyEventEffect(player);
+        assertEquals(10,player.getFood());
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.ARTIST));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.BUILDER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.HUNTER));
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.INVENTOR));
+        buildingCard.applyEventEffect(player);
+        assertEquals(10,player.getFood());
+        player.addCardToTribe(new ArtistCard(ERA.ERA_III,CARD_TYPE.SHAMAN));
+        buildingCard.applyEventEffect(player);
+        assertEquals(15,player.getFood());
+
+    }
+
+}
