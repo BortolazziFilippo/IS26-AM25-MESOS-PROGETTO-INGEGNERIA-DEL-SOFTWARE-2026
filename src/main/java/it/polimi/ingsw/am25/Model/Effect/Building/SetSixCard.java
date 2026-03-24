7dd5753bf8e78b6601885cb48920bb859f8e7b55
sara <sara.caliment@mail.polimi.it>
2026-03-24 12:53:36 +0100

    scritto il metodo applyEffect di SetSixcard
    (cherry picked from commit 91228dea27fb6ed80993989a7e965ba3d67786b4)

--- src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SetSixCard.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SetSixCard.java
@@ -1,15 +1,63 @@
 package it.polimi.ingsw.am25.Model.Effect.Building;
 
+import it.polimi.ingsw.am25.Model.Card.Card;
 import it.polimi.ingsw.am25.Model.Player.Player;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class SetSixCard extends BuildingEffect{
     private final int foodToGive = 5;
+    private List<Card> oldCards = new ArrayList<>();
 
     public SetSixCard() {
     }
 
     @Override
     public void applyEffect(Player player) {
+        int setCompleti;
+        List <Card> newCards= new ArrayList<>(player.getTribe());
+        List <Integer> setCards = new ArrayList<>();
+        newCards.removeAll(oldCards);
+        for (int i = 0; i < 6; i++) {
+            setCards.add(i,0);
+        }
+        for (Card card : newCards) {
+            switch (card.getCardType()){
+                case BUILDER:
+                    setCards.addFirst(setCards.getFirst()+1);
+                    break;
+                case ARTIST:
+                    setCards.add(1,setCards.get(1)+1);
+                    break;
+                case GATHERER:
+                    setCards.add(2,setCards.get(2)+1);
+                    break;
+                case SHAMAN:
+                    setCards.add(3,setCards.get(3)+1);
+                    break;
+                case INVENTOR:
+                    setCards.add(4,setCards.get(4)+1);
+                    break;
+                case HUNTER:
+                    setCards.add(5,setCards.get(5)+1);
+                    break;
+                default:
+                    System.err.println(getClass()+" errore identificazione carta");
+
+            }
+
+        }
+        oldCards = newCards;
+        newCards.clear();
+        if(!setCards.contains(0)){
+            setCompleti = Collections.min(setCards);
+            player.manageFood(foodToGive*setCompleti);
+            setCards.clear();
+        }
 
     }
 }
