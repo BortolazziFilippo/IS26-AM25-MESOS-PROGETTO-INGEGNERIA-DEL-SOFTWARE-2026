893b783d3f2c5a3074c7ad771310cc1b5594f14c
Filippo Bortolazzi <filippo.bortolazzi@mail.polimi.it>
2026-03-24 16:51:25 +0100

    Creata Utilities per spostare funzioni molto usate sopra

--- src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SetSixCard.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SetSixCard.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.am25.Model.Effect.Building;
 
 import it.polimi.ingsw.am25.Model.Card.Card;
 import it.polimi.ingsw.am25.Model.Player.Player;
+import it.polimi.ingsw.am25.Model.Utilities;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,38 +20,11 @@ public class SetSixCard extends BuildingEffect{
     @Override
     public void applyEffect(Player player) {
         int setCompleti;
+        int quantity=6;
         List <Card> newCards= new ArrayList<>(player.getTribe());
-        List <Integer> setCards = new ArrayList<>();
+        List <Integer> setCards = new ArrayList<>(Collections.nCopies(quantity,0));
         newCards.removeAll(oldCards);
-        for (int i = 0; i < 6; i++) {
-            setCards.add(i,0);
-        }
-        for (Card card : newCards) {
-            switch (card.getCardType()){
-                case BUILDER:
-                    setCards.set(0, setCards.getFirst()+1);
-                    break;
-                case ARTIST:
-                    setCards.set(1,setCards.get(1)+1);
-                    break;
-                case GATHERER:
-                    setCards.set(2,setCards.get(2)+1);
-                    break;
-                case SHAMAN:
-                    setCards.set(3,setCards.get(3)+1);
-                    break;
-                case INVENTOR:
-                    setCards.set(4,setCards.get(4)+1);
-                    break;
-                case HUNTER:
-                    setCards.set(5,setCards.get(5)+1);
-                    break;
-                default:
-                    System.err.println(getClass()+" errore identificazione carta");
-
-            }
-
-        }
+        Utilities.countOccurrence(newCards,setCards);
         oldCards = newCards;
         newCards.clear();
         if(!setCards.contains(0)){
--- src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SixFoodCompletedSet.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/SixFoodCompletedSet.java
@@ -2,8 +2,10 @@ package it.polimi.ingsw.am25.Model.Effect.Building;
 
 import it.polimi.ingsw.am25.Model.Card.Card;
 import it.polimi.ingsw.am25.Model.Player.Player;
+import it.polimi.ingsw.am25.Model.Utilities;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 //1
 //2
@@ -13,7 +15,7 @@ import java.util.List;
 //6
 public class SixFoodCompletedSet extends BuildingEffect {
     private boolean alreadyUsed = false;
-    private final List<Integer> setCard = new ArrayList<>();
+    private  List<Integer> setCard;
     private List<Card> listOldCard;
 
     public SixFoodCompletedSet() {
@@ -23,10 +25,8 @@ public class SixFoodCompletedSet extends BuildingEffect {
     public void applyEffect(Player player) {
 
         if(!alreadyUsed){
-            for (int i = 0; i < 6; i++) {
-                setCard.add(i,0);
-            }
-
+            int quantity=6;
+            setCard = new ArrayList<>(Collections.nCopies(quantity,0));
             listOldCard=new ArrayList<>(player.getTribe());
             this.alreadyUsed=true;
         }else{
@@ -34,32 +34,10 @@ public class SixFoodCompletedSet extends BuildingEffect {
             List<Card> difference = new ArrayList<>(listOfNewCard);
             difference.removeAll(listOldCard);
             listOldCard=new ArrayList<>(listOfNewCard);
-            for (Card card: difference){
-                switch (card.getCardType()){
-                    case BUILDER:
-                        setCard.set(0,setCard.getFirst()+1);
-                        break;
-                    case ARTIST:
-                        setCard.set(1,setCard.get(1)+1);
-                        break;
-                    case GATHERER:
-                        setCard.set(2,setCard.get(2)+1);
-                        break;
-                    case SHAMAN:
-                        setCard.set(3,setCard.get(3)+1);
-                        break;
-                    case INVENTOR:
-                        setCard.set(4,setCard.get(4)+1);
-                        break;
-                    case HUNTER:
-                        setCard.set(5,setCard.get(5)+1);
-                        break;
-                    default:
-                        System.err.println(getClass()+" errore identificazione carta");
-                }
-            }
 
-            if(!setCard.contains(0)){
+            Utilities.countOccurrence(difference, setCard);
+
+            while (!setCard.contains(0)){
                 player.manageFood(5);
                 setCard.replaceAll(integer -> integer-1);
             }
