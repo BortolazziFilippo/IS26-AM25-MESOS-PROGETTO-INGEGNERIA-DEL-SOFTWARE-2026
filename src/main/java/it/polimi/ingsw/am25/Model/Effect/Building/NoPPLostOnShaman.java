8f3513b575f64e3eae23ef6640c38207d9444efb
Robert Gabriel Anton <robertgabriel.anton@mail.polimi.it>
2026-03-24 15:50:14 +0100

    changes commit

--- src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/NoPPLostOnShaman.java
+++ src/main/java/it/polimi/ingsw/am25/Model/Effect/Building/NoPPLostOnShaman.java
@@ -3,14 +3,14 @@ package it.polimi.ingsw.am25.Model.Effect.Building;
 import it.polimi.ingsw.am25.Model.Player.Player;
 
 public class NoPPLostOnShaman extends BuildingEffect{
-    private final int PPToRestore;
 
-    public NoPPLostOnShaman(int PPToRestore) {
-        this.PPToRestore = PPToRestore;
+
+    public NoPPLostOnShaman() {
+
     }
 
     @Override
     public void applyEffect(Player player) {
-        player.managePP(PPToRestore);
+
     }
 }
