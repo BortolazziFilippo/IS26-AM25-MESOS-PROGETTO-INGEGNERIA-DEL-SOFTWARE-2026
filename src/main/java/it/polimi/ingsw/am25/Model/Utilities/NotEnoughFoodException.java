fb06057a258addc3e654186407b4b2ca24213a87
Filippo Bortolazzi <filippo.bortolazzi@mail.polimi.it>
2026-03-25 12:11:08 +0100

    Aggiunte funzioni utilities e classe utilities constant, creata eccezione personalizzata NotEnoughFoodException

---
+++ src/main/java/it/polimi/ingsw/am25/Model/Utilities/NotEnoughFoodException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.am25.Model.Utilities;
+
+public class NotEnoughFoodException extends RuntimeException{
+    public NotEnoughFoodException(String message) {
+        super(message);
+    }
+}
